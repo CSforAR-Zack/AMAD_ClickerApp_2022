09d5817ccc099ec47d5206ec4d6c0233ed2529ec
Zackary <zspink@nea.k12.ar.us>
2022-07-12 12:44:21 -0500

    First Commit

---
+++ ClickerApp/ClickerApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ClickerApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void CounterAdd_Clicked(object sender, EventArgs e)
+	{
+		count++;
+		counterLabel.Text = $"Count: {count}";
+	}
+}
+
